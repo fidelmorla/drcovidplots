53efb2762975d91c71a06b86efcf9f0ae64526fd
fidelmorla <fidel.morla@gmail.com>
2020-05-14 14:16:35 -0400

    Initial commit

--- R/drcovidplots-package.R
+++ R/drcovidplots-package.R
@@ -1,15 +1,17 @@
-#' @keywords internal
-"drcovidplots"
+#' @title package-drcovidplots
+#' @name drcovidplots
+#' @keywords internal "drcovidplots"
 #' @aliases drcovidplots
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
-#' @description: This packages generates several plots using COVID-19 data from the Minister of Public Health and Social Assintance from Dominican Republic for COVID19.
+#' @export
+#' @description: This packages generates several plots using COVID-19 data from
+#' the Minister of Public Health and Social Assintance from Dominican Republic for COVID19.
 #' @section Warning:
 #' Before using any function, you need to run,
 #' in order, \code{load_data_covid_dr()} and \code{load_themes()}
-
 
 
 NULL
