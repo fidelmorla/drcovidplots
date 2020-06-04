8b2dfbb36d061495e6bc2949b5607cd152058fa1
fidelmorla <fidel.morla@gmail.com>
2020-06-04 14:09:04 +0000

    Last updated: June 04, 2020

--- R/libraries.R
+++ R/libraries.R
@@ -1,6 +1,6 @@
 #' @title Librerias
 #' @description Dependencies
-#' @import  googlesheets4 ggplot2 dplyr tidyr purrr stringr magrittr gganimate
+#' @import  ggplot2 dplyr tidyr purrr stringr magrittr gganimate
 #' @import readxl lubridate XML rvest xml2 grDevices
 #' @import ggalt ggthemes gridExtra grid forcats gifski colorspace ggrepel lemon
 #' @importFrom stats cor reorder
