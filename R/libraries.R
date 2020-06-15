ca702d827d3836c22858cd34aecca57a99331574
fidelmorla <fidel.morla@gmail.com>
2020-06-15 16:23:58 +0000

    package sf included

--- R/libraries.R
+++ R/libraries.R
@@ -1,7 +1,7 @@
 #' @title Librerias
 #' @description Dependencies
 #' @import  ggplot2 dplyr tidyr purrr stringr magrittr gganimate
-#' @import lubridate XML rvest xml2 grDevices httr
+#' @import lubridate XML rvest xml2 grDevices httr sf
 #' @import ggalt ggthemes gridExtra grid forcats gifski colorspace ggrepel lemon
 #' @importFrom stats cor reorder
 #' @importFrom utils tail
